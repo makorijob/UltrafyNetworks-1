7e932d17c5a68c0e0db7d078f53a5e25311d86bd
makorijob <146344763+makorijob@users.noreply.github.com>
2026-06-28 01:42:19 +0300

    Update base URL in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://ultrafynetwork.makorijob760.workers.dev";
+  const baseUrl = "https://ultrafynetwork.onpointtech.workers.dev";
 
   return [
     {
